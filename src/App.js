b929d305d46a758449cbf403218fa65ae54bea37
Samuel Ruiz <samuelpuckett77@gmail.com>
2020-01-22 20:36:58 -0800

    Lecture 29: Card List Component
    - we added a card-list.component.jsx file in which we are taking in props form its parents and rendering a <div> element w/ it's children prop using {props.children}
    - We also are passing in the .map() over the monsters array from state which we are populating from the fetch call to the jsonplaceholder api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { CardList } from "./components/card-list/card-list.component";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: "Hello Sam"
+      monsters: []
     };
   }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>{this.state.string}</p>
-          <button
-            onClick={() => {
-              this.setState({ string: "Hello World" });
-            }}
-          >
-            Change text
-          </button>
-        </header>
+        <CardList name='Sam'>
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
       </div>
     );
   }
