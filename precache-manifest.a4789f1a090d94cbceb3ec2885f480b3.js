2128834ca22eb7a8f62fca899c4001d38f5c4c97
Samuel Ruiz <samuelpuckett77@gmail.com>
2020-01-25 00:05:31 -0800

    Updates

---
+++ precache-manifest.a4789f1a090d94cbceb3ec2885f480b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "481322c4a71b01afa02420de1b0f6474",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4b3622598c403060a313",
+    "url": "/monsters-rolodex/static/css/main.7aa0a2cd.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b3622598c403060a313",
+    "url": "/monsters-rolodex/static/js/main.f9af5920.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
